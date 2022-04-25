c0190827796dbc51b50b01c684a4fe30701424ad
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-25 15:35:35 -0700

    game logic added

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ If you work on more than one feature at a time, you are guaranteed to multiply y
 1) **Look at the drawing and imagine using the app. What _state_ do you need to track?** We need to track the loss, wins and total spans
 1) **For each HTML element ask: Why do I need this? (i.e., "we need div to display the results in")** 
 1) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How" (i.e., `resultsEl.textContent = newResults`)**
-  We need to grab the DOMS for each element using getElementById. We need to hide the ball div in css and use reveal. 
+  We need to grab the DOMS for each element using getElementById. We need to hide the ball div in css and use reveal. We use addEventListener to grab the users clicks on each button.  
 1) **Find all the 'events' (user clicks, form submit, on load etc) in your app. Ask one by one, "What happens when" for each of these events. Does any state change? Does any DOM update?**
   when the user guesses, wins and/or losses needs to change. Total will be relative to wins and losses.
 1) **Think about how to validate each of your features according to a Definition of Done. (Hint: console.log usually helps here.)**
--- app.js
+++ app.js
@@ -1,8 +1,67 @@
 // import functions and grab DOM elements
+const buttonOne = document.getElementById ('button1');
+const buttonTwo = document.getElementById ('button2');
+const buttonThree = document.getElementById ('button3');
+
+const winSpan = document.getElementById ('wins');
+const lossSpan = document.getElementById ('losses');
+const totalSpan = document.getElementById ('total');
+
+const ballOne = document.getAnimations ('hidden1');
+const ballTwo = document.getAnimations ('hidden2');
+const ballThree = document.getAnimations ('hidden3');
 
 // let state
+let wins = 0;
+let losses = 0;
 
+//console.log(buttonOne, buttonTwo, buttonThree, winSpan, lossSpan, totalSpan);
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  // if button 1 = the random number, increase wins by one. ***later*** hide and reveal the ball
+//const randomGuess = Math.ceil(Math.random() * 3); 
+
+buttonOne.addEventListener('click', () => {
+    const randomGuess = Math.ceil(Math.random() * 3);
+    
+    if (randomGuess === 1) {
+        wins++;
+    } else { 
+        losses++;
+    }
+
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+});  
+
+buttonTwo.addEventListener('click', () => {
+    const randomGuess = Math.ceil(Math.random() * 3);
+  
+    if (randomGuess === 1) {
+        wins++;
+    } else { 
+        losses++;
+    }
+
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+});  
+
+buttonThree.addEventListener('click', () => {
+    const randomGuess = Math.ceil(Math.random() * 3);
+  
+    if (randomGuess === 1) {
+        wins++;
+    } else { 
+        losses++;
+    }
+
+    winSpan.textContent = wins;
+    lossSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+});  
