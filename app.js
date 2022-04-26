f636a5e7f4b9ef3936ef550e27a13bc4f2a40a2a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-25 18:37:44 -0700

    Added minor CSS styling

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-// import functions and grab DOM elements
 const buttonOne = document.getElementById ('button1');
 const buttonTwo = document.getElementById ('button2');
 const buttonThree = document.getElementById ('button3');
-
 const winSpan = document.getElementById ('wins');
 const lossSpan = document.getElementById ('losses');
 const totalSpan = document.getElementById ('total');
-
 const shellOne = document.getElementById ('shell-1');
 const shellTwo = document.getElementById ('shell-2');
 const shellThree = document.getElementById ('shell-3');
-// let state
+
 let wins = 0;
 let losses = 0;
 
-//console.log(buttonOne, buttonTwo, buttonThree, winSpan, lossSpan, totalSpan);
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-
-  // if button 1 = the random number, increase wins by one. ***later*** hide and reveal the ball
-//const randomGuess = Math.ceil(Math.random() * 3); 
 buttonOne.addEventListener('click', () => {
     shellOne.classList.remove('reveal');
     shellTwo.classList.remove('reveal');
@@ -31,18 +20,15 @@ buttonTwo.addEventListener('click', () => {
     shellOne.classList.remove('reveal');
     shellTwo.classList.remove('reveal');
     shellThree.classList.remove('reveal');
-}
-);
+});
 buttonThree.addEventListener('click', () => {
     shellOne.classList.remove('reveal');
     shellTwo.classList.remove('reveal');
     shellThree.classList.remove('reveal');
-}
-);
+});
 
 buttonOne.addEventListener('click', () => {
     const randomGuess = Math.ceil(Math.random() * 3);
-
     if (randomGuess === 1) {
         wins++;
         shellOne.classList.add('reveal');
@@ -53,16 +39,13 @@ buttonOne.addEventListener('click', () => {
         losses++;
         shellThree.classList.add('reveal');
     }
-
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
     totalSpan.textContent = wins + losses;
 });  
 
-
 buttonTwo.addEventListener('click', () => {
     const randomGuess = Math.ceil(Math.random() * 3);
-
     if (randomGuess === 2) {
         wins++;
         shellTwo.classList.add('reveal');
@@ -73,7 +56,6 @@ buttonTwo.addEventListener('click', () => {
         losses++;
         shellThree.classList.add('reveal');
     }
-
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
     totalSpan.textContent = wins + losses;
@@ -81,7 +63,6 @@ buttonTwo.addEventListener('click', () => {
 
 buttonThree.addEventListener('click', () => {
     const randomGuess = Math.ceil(Math.random() * 3);
-
     if (randomGuess === 3) {
         wins++;
         shellThree.classList.add('reveal');
@@ -92,7 +73,6 @@ buttonThree.addEventListener('click', () => {
         losses++;
         shellOne.classList.add('reveal');
     }
-
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
     totalSpan.textContent = wins + losses;
